7cc94e27836c5bb2c4c74114128267871e9b9732
avanishkumar5035 <146574431+avanishkumar5035@users.noreply.github.com>
2025-10-13 14:59:17 +0530

    Initial commit

--- L16/Intro to js/objects.js
+++ L16/Intro to js/objects.js
@@ -1,11 +1,29 @@
-console.log("hello")
-// var let const
+var student = {
+    name : "avanish",
+    age : 18,
+    city : "mainpuri",
+    sem:5,
+    marks:[6.5,6.3,6.4,6.4],
+    key:34
+}
+console.log(student);
+console.log(student.marks);
+console.log(student.name);
+console.log(student.age);
 
-var x= 46;
+//  for loop
 
-console.log(x);
-let x = 10;
-console.log(x);
-x = 20;
-const x =10;
-console.log(x);
+var arr = [34,56,78,19];
+for( var i = 0; i<arr.length;i++){
+    console.log(arr[i]);
+}
+
+for( let item of arr){
+    console.log(item);
+}
+
+for(let key in student){
+    console.log(key);
+    // console.log(student.key);
+    console.log(student[key]);
+}
